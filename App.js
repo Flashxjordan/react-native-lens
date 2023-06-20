5331c018d96e66d5ff4c2161f1785325c1cc0ad5
David Jordan <wordnerd92@gmail.com>
2023-06-20 10:28:25 -0500

    created the home screen

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Profiles } from '@lens-protocol/react-native-lens-ui-kit';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Profiles } from "@lens-protocol/react-native-lens-ui-kit";
 
-export default function App() {
+const Stack = createStackNavigator()
+
+
+export default function App(){
   return (
-      <Profiles />
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ProfileComponent} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function ProfileComponent({
+  navigation
+}) {
+  return (
+    <Profiles 
+
+    />
+  )
+}
