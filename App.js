8525f7977662ec637101b476994af46be9373c5a
David Jordan <wordnerd92@gmail.com>
2023-06-20 11:33:14 -0500

    getting an error message while clicking on profiles

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet } from 'react-native';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native'
 import { createStackNavigator } from "@react-navigation/stack";
-import { Profiles } from "@lens-protocol/react-native-lens-ui-kit";
+import { LensProvider, Theme } from '@lens-protocol/react-native-lens-ui-kit';
+
+import Profiles from "/Users/user/Desktop/react-native-lens/Profiles.js"
+import ProfileView from "/Users/user/Desktop/react-native-lens/ProfileView.js"
+import ViewComments from "/Users/user/Desktop/react-native-lens/ViewComments.js"
+import ViewFollowing from "/Users/user/Desktop/react-native-lens/ViewFollowing.js"
 
 const Stack = createStackNavigator()
 
-
-export default function App(){
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={ProfileComponent} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+    <LensProvider theme={Theme.dark}>
+      <NavigationContainer theme={DarkTheme}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Profiles}  />
+          <Stack.Screen name="ViewProfile" component={ProfileView} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </LensProvider>
+  );
 }
 
-function ProfileComponent({
-  navigation
-}) {
-  return (
-    <Profiles 
-
-    />
-  )
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
